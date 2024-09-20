f970d814118038aea3f035780655b18ba98df24e
The-Mag-Pie <barteksor2000@onet.pl>
2024-09-20 02:34:54 +0200

    refactor stove controller

--- MagPie-Home-Control-API/Controllers/CentralHeating/StoveController.cs
+++ MagPie-Home-Control-API/Controllers/CentralHeating/StoveController.cs
@@ -1,17 +1,18 @@
-﻿using MagPie_Home_Control_API.Models;
+﻿using MagPie_Home_Control_API.Enums;
+using MagPie_Home_Control_API.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MagPie_Home_Control_API.Controllers.CentralHeating.Stove
+namespace MagPie_Home_Control_API.Controllers.CentralHeating
 {
-    [Route("centralheating/stove/toggle")]
+    [Route("centralheating/stove")]
     [ApiController]
-    public class ToggleController : ControllerBase
+    public class StateController : ControllerBase
     {
         private readonly string _ewelinkProxyUrl;
         private readonly string _ewelinkDeviceId;
         private readonly HttpClient _httpClient;
 
-        public ToggleController(IConfiguration configuration, HttpClient httpClient)
+        public StateController(IConfiguration configuration, HttpClient httpClient)
         {
             _ewelinkProxyUrl = configuration["EWELINK_PROXY_URL"] ?? throw new EnvironmentVariableMissingException("EWELINK_PROXY_URL");
             _ewelinkDeviceId = configuration["EWELINK_DEVICE_ID"] ?? throw new EnvironmentVariableMissingException("EWELINK_DEVICE_ID");
@@ -19,11 +20,41 @@ namespace MagPie_Home_Control_API.Controllers.CentralHeating.Stove
         }
 
         /// <summary>
-        /// Toggle stove power state
+        /// Get stove power state
         /// </summary>
         [HttpGet]
         [ApiExplorerSettings(GroupName = "Central heating")]
         public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var ewelinkResponse = await _httpClient.GetStringAsync($"{_ewelinkProxyUrl}/state/{_ewelinkDeviceId}");
+
+                if (ewelinkResponse == "on")
+                {
+                    return Ok(new StateResponse(PowerStates.ON));
+                }
+                else if (ewelinkResponse == "off")
+                {
+                    return Ok(new StateResponse(PowerStates.OFF));
+                }
+                else
+                {
+                    throw new ServerErrorException($"Invalid response from eWeLink: {ewelinkResponse}");
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ApiResponseBody(false, e.Message));
+            }
+        }
+
+        /// <summary>
+        /// Toggle stove power state
+        /// </summary>
+        [HttpPost]
+        [ApiExplorerSettings(GroupName = "Central heating")]
+        public async Task<IActionResult> Post()
         {
             try
             {
