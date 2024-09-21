06a7440a3c66ed5316bc57c4df11eddb90573543
The-Mag-Pie <barteksor2000@onet.pl>
2024-09-21 03:44:26 +0200

    add temporary api key authorization

---
+++ MagPie-Home-Control-API/Authorization/ApiKeyMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using MagPie_Home_Control_API.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Text.Json;
+
+namespace MagPie_Home_Control_API.Authorization
+{
+    // Temporary solution for endpoint security
+    public class ApiKeyMiddleware : IMiddleware
+    {
+        private readonly string _apiKey;
+
+        public ApiKeyMiddleware(IConfiguration configuration)
+        {
+            _apiKey = configuration["API_KEY"] ?? throw new EnvironmentVariableMissingException("API_KEY");
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            context.Response.Headers.Append("Access-Control-Allow-Origin", "*");
+
+            // preflight
+            if (context.Request.Method == HttpMethods.Options)
+            {
+                context.Response.StatusCode = 204;
+                context.Response.Headers.Append("Access-Control-Allow-Headers", "X-Api-Key");
+                return;
+            }
+
+            if (!context.Request.Headers.TryGetValue("X-Api-Key", out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                context.Response.ContentType = "application/problem+json; charset=utf-8";
+
+                var response = new ApiResponseBody(false, "API key not found");
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                return;
+            }
+
+            var apiKey = extractedApiKey.ToString();
+            if (apiKey != _apiKey)
+            {
+                context.Response.StatusCode = 401;
+                context.Response.ContentType = "application/problem+json; charset=utf-8";
+
+                var response = new ApiResponseBody(false, "Invalid API key");
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                return;
+            }
+
+            await next(context);
+        }
+    }
+}
--- MagPie-Home-Control-API/Program.cs
+++ MagPie-Home-Control-API/Program.cs
@@ -1,5 +1,6 @@
 
 using System.Reflection;
+using MagPie_Home_Control_API.Authorization;
 
 namespace MagPie_Home_Control_API
 {
@@ -39,10 +40,36 @@ namespace MagPie_Home_Control_API
                 });
 
                 c.DocInclusionPredicate((name, api) => true);
+
+                c.AddSecurityDefinition("ApiKey", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                    Name = "X-Api-Key",
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                    Description = "API key"
+                });
+
+                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+                {
+                    {
+                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                        {
+                            Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                            {
+                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                                Id = "ApiKey"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
             });
 
             // Add HTTP client
             builder.Services.AddHttpClient();
+
+            // Add middleware
+            builder.Services.AddSingleton<ApiKeyMiddleware>();
 
             var app = builder.Build();
 
@@ -55,6 +82,7 @@ namespace MagPie_Home_Control_API
 
             app.UseAuthorization();
 
+            app.UseMiddleware<ApiKeyMiddleware>();
 
             app.MapControllers();
 
